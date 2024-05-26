04df6c97fe4ebd2fd1adcb6dc11e0ea2dbe96d6f
Humberto Fernández <humbertof44@gmail.com>
2024-05-26 13:46:49 -0400

    Implement Pico CSS in React with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <main style={{padding: '2rem'}}>
+        <div className="container">
+          <header>
+            <h1>Hola, Pico CSS con React</h1>
+          </header>
+          <main>
+            <p>Este es un ejemplo de cómo usar Pico CSS en un proyecto de React.</p>
+            <button className="primary">Haz clic aquí</button>
+          </main>
+        </div>
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+import '@picocss/pico'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
